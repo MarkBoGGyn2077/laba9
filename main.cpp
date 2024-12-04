a0712a30e22aab856c96aeef83aa8253b6563d7d
mark <zingotv083@gmail.com>
2024-12-04 10:49:55 +0300

    Добавлен ввод двух целых чисел с приглашением

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int num1, num2;
+
+   
+    std::cout << "Enter the first integer: ";
+    std::cin >> num1;
+
+    
+    std::cout << "Enter the second integer: ";
+    std::cin >> num2;
+
+    // Вывод введенных чисел
+    std::cout << "You entered the numbers: " << num1 << " and " << num2 << std::endl;
+
+    return 0;
+}
