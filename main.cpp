8f81481f5508f08f5d2ec962c49e1e0708b490dd
mark <zingotv083@gmail.com>
2024-12-08 17:29:04 +0300

    2.5.1 изменение с разностью

--- main.cpp
+++ main.cpp
@@ -14,9 +14,10 @@ int main() {
 
     // Вывод введенных чисел
     std::cout << "You entered the numbers: " << num1 << " and " << num2 << std::endl;
+    int difference = a - b;
     int sum = num1 + num2;
     std::cout << "symma " << sum << std::endl;
-
+    std::cout << "Difference:" << difference << std::endl;
 
     return 0;
 }
